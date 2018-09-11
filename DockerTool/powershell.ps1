56724b34927740730767fb893a62ecf408716fbf
Dmitryi <33095183+PREPAREFORBATTLE@users.noreply.github.com>
2018-09-11 16:11:23 +0300

    Add files via upload

--- DockerTool/powershell.ps1
+++ DockerTool/powershell.ps1
@@ -41,13 +41,29 @@ while($containername -notlike "exit") {
 }
 docker start $containername
 $version = docker exec $containername cat /etc/issue.net
+$version1 = docker exec $containername cat /etc/redhat-release
+$version2 = docker exec $containername cat /etc/issue
 if ($version -match "Ubuntu"){
 	$version = $version.Remove(12)
 }
 if ($version -match "Debian"){
 	$version = "Debian " + $version.Substring($version.Length -1, 1)
 }
-
+if ($version1 -match "CentOS"){
+	$version = $version1.Remove(6)
+}
+if ($version1 -match "openSUSE"){
+	$version = "openSUSE"
+}
+if ($version1 -match "Red Hat"){
+	$version = $version1.Remove(7)
+}
+if ($version1 -match "Fedora"){
+	$version = "Fedora"
+}
+if ($version2 -match "Arch Linux"){
+	$version = "Arch Linux"
+}
 switch ($version){
 	"Ubuntu 16.04" {
 		docker exec $containername apt update
@@ -99,6 +115,48 @@ switch ($version){
 		docker exec $containername sudo curl -o /etc/apt/sources.list.d/microsoft.list https://packages.microsoft.com/config/debian/8/prod.list
 		docker exec $containername sudo apt-get update
 		docker exec $containername sudo apt-get install -y powershell
+	}
+	"CentOS" {
+		docker exec $containername yum update -y
+		docker exec $containername yum install -y apt-utils ca-certificates curl apt-transport-https sudo wget
+		docker exec $containername sudo curl -o /etc/yum.repos.d/microsoft.repo https://packages.microsoft.com/config/rhel/7/prod.repo
+		docker exec $containername sudo yum install -y powershell
+	}
+	#need to register	
+	<#"Red Hat"{
+		docker exec $containername 
+		docker exec $containername
+		docker exec $conatinername
+		docker exec $containername
+	}#>
+	"Ubuntu 12.04" {
+		"_________________________________________"
+		"Your version of ubuntu does not support powershell"
+		"_________________________________________"
+	}
+
+	#Of the two proposed solutions, you need to automatically select the second(?)
+	"OpenSUSE"{
+		docker exec $containername zypper install -y sudo wget
+		docker exec $containername sudo wget https://packages.microsoft.com/keys/microsoft.asc
+		docker exec $containername zypper ar https://packages.microsoft.com/rhel/7/prod/ rep
+		docker exec $containername sudo zypper update -y
+		docker exec $containername sudo zypper install powershell
+	}
+	"Fedora"{
+		docker exec $containername dnf install -y sudo
+		docker exec $containername sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+		docker exec $containername curl -o /etc/yum.repos.d/microsoft.repo https://packages.microsoft.com/config/rhel/7/prod.repo
+		docker exec $containername sudo dnf update -y
+		docker exec $containername sudo dnf install -y compat-openssl10
+		docker exec $containername sudo dnf install -y powershell
+	}
+	#with root can't do anything
+	"Arch Linux"{
+		docker exec $containername pacman -Sy
+		docker exec $containername pacman -S -y sudo
+		docker exec $containername
+		docker exec $containername
 	}
 }
 
